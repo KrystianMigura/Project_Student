bbdbfa615907788f4ec7a56919e5e7d616987e6b
KrystianMigura <37398255+KrystianMigura@users.noreply.github.com>
2018-12-08 11:02:53 +0100

    z palca
    xxx

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -28,19 +28,19 @@ public class Controller implements Initializable {
     @FXML
     public Button edycjaWiersza;
     @FXML
-    public TableView <users> table_info;
+    public TableView<users> table_info;
     @FXML
-    private TableColumn <users, String> col_id;
+    private TableColumn<users, String> col_id;
     @FXML
-    private TableColumn <users, String> col_imie;
+    private TableColumn<users, String> col_imie;
     @FXML
-    private TableColumn <users, String> col_Nazwisko;
+    private TableColumn<users, String> col_Nazwisko;
     @FXML
-    private TableColumn <users, String> col_Tel;
+    private TableColumn<users, String> col_Tel;
     @FXML
-    private TableColumn <users, String> col_Pesel;
+    private TableColumn<users, String> col_Pesel;
     @FXML
-    private TableColumn <users, String> col_Uprawnienia;
+    private TableColumn<users, String> col_Uprawnienia;
     @FXML
     public TextField number_text;
     @FXML
@@ -59,11 +59,11 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle sample) {
-        col_id.setCellValueFactory(new PropertyValueFactory <>("id"));
-        col_imie.setCellValueFactory(new PropertyValueFactory <>("FirstName"));
-        col_Nazwisko.setCellValueFactory(new PropertyValueFactory <>("surname"));
-        col_Pesel.setCellValueFactory(new PropertyValueFactory <>("pes"));
-        col_Tel.setCellValueFactory(new PropertyValueFactory <>("nr_tel"));
+        col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col_imie.setCellValueFactory(new PropertyValueFactory<>("FirstName"));
+        col_Nazwisko.setCellValueFactory(new PropertyValueFactory<>("surname"));
+        col_Pesel.setCellValueFactory(new PropertyValueFactory<>("pes"));
+        col_Tel.setCellValueFactory(new PropertyValueFactory<>("nr_tel"));
         col_Uprawnienia.setCellValueFactory(new PropertyValueFactory<>("typ"));
 
 
@@ -79,7 +79,7 @@ public class Controller implements Initializable {
         edycjaWiersza.setDisable(true);
         //typ.getItems().addAll("Administrator","Moderator","Programista", "Tester", "Użytkownik");
 
-                }
+    }
 
    /* public void enableButton(){
         if (table_info.getSelectionModel().getSelectedItem() != null)
@@ -110,14 +110,14 @@ public class Controller implements Initializable {
         }
     }
 
-public void ButtonBack(){
-    Methods met = new Methods();
-    Add_Button.setDisable(false);
-    if(edycjaWiersza.getText() == "Edycja ON" || edycjaWiersza.getText() == "Edycja OFF")
-    edycjaWiersza.setDisable(false);
+    public void ButtonBack() {
+        Methods met = new Methods();
+        Add_Button.setDisable(false);
+        if (edycjaWiersza.getText() == "Edycja ON" || edycjaWiersza.getText() == "Edycja OFF")
+            edycjaWiersza.setDisable(false);
 
-    met.Tableinback(table_info, surname_text,  FirstName_text, nr_tel, pes_text, number_text, button1test, buttonback, typ);
-   // typ.setLayoutY(900);
+        met.Tableinback(table_info, surname_text, FirstName_text, nr_tel, pes_text, number_text, button1test, buttonback, typ);
+        // typ.setLayoutY(900);
     }
 
 
@@ -125,94 +125,281 @@ public void ButtonBack(){
         Function a = new Function();
         a.checkID(number_text);
     }
-    public void checkPes(){
+
+    public void checkPes() {
         Function b = new Function();
         b.checkPes(pes_text);
     }
 
-    public void checktel(){
+    public void checktel() {
         Function c = new Function();
         c.checkTel(nr_tel);
     }
 
-    public void AddButton1()  {
+    public void AddButton1() {
 
-    Function testowa = new Function();
-    testowa.testprzycuski(number_text,FirstName_text,surname_text, pes_text,nr_tel,typ, table_info, edycjaWiersza);
+        Function testowa = new Function();
+        testowa.testprzycuski(number_text, FirstName_text, surname_text, pes_text, nr_tel, typ, table_info, edycjaWiersza);
 
     }
 
 
-public ObservableList<users> loadData(){
-      ObservableList<users> data_table =FXCollections.observableArrayList();
+    public ObservableList<users> loadData() {
+        ObservableList<users> data_table = FXCollections.observableArrayList();
 
-       return data_table;
-  }
+        return data_table;
+    }
 
-    public void EdycjaWiersza(){
+    public void EdycjaWiersza() {
 
-        ObservableList <users> person;
+        ObservableList<users> person;
         person = table_info.getSelectionModel().getSelectedItems();
 
-        if(edycjaWiersza.getText()=="Edycja OFF"){
+        if (edycjaWiersza.getText() == "Edycja OFF") {
             edycjaWiersza.setText("Edycja ON");
             table_info.setDisable(false);
-            }
-            else
-            {
-                edycjaWiersza.setText("Edycja OFF");
-                table_info.setDisable(true);
-            }
+        } else {
+            edycjaWiersza.setText("Edycja OFF");
+            table_info.setDisable(true);
+        }
 
 
-
-
-        if(table_info.getSelectionModel().getSelectedItem() != null) {
+        if (table_info.getSelectionModel().getSelectedItem() != null) {
             System.out.println("Imie: " + person.get(0).getFirstName() + " Nazwisko: " + person.get(0).getSurname());
 
         }
     }
 
-// edycja komorki imie
-    public void onEditImie(TableColumn.CellEditEvent<users, String> usersStringCellEditEvent) {
+    boolean cETN = false;
+    String sprImie,sprId, sprSurname,sprPes,sprTel,sprPosition;
+    String oldVal;
+
+    // edycja komorki imie
+    public void onEditImie() {
+        checkEditImie();
         users user = table_info.getSelectionModel().getSelectedItem();
-        user.setFirstName(usersStringCellEditEvent.getNewValue());
+        oldVal = user.getFirstName();
+        checkImieEdit(sprImie);
+        if(cETN == true && sprImie.length() > 0){
+            user.setFirstName(sprImie);
+        }else{
+            user.setFirstName(oldVal);
+        }
+        table_info.refresh();
     }
 
-    public void onEditId(TableColumn.CellEditEvent<users, String> usersStringCellEditEvent) {
+    public void onEditId() {
+        checkEditId();
         users user = table_info.getSelectionModel().getSelectedItem();
-        user.setId(usersStringCellEditEvent.getNewValue());
+        oldVal = user.getId();
+        checkIdEdit(sprId);
+        if (cETN == true && sprId.length() > 0) {
+            user.setId(sprId);
+        } else {
+            user.setId(oldVal);
+        }
+        table_info.refresh();
     }
 
-    public void OnEditSurname(TableColumn.CellEditEvent<users, String> usersStringCellEditEvent) {
+    public void OnEditSurname() {
+        checkEditSurname();
         users user = table_info.getSelectionModel().getSelectedItem();
-        user.setSurname(usersStringCellEditEvent.getNewValue());
+        oldVal = user.getSurname();
+        checkSurnameEdit(sprSurname);
+        if (cETN == true && sprSurname.length() > 0) {
+            user.setSurname(sprSurname);
+        } else {
+            user.setSurname(oldVal);
+        }
+        table_info.refresh();
     }
 
-
-    public void onEditPes(TableColumn.CellEditEvent<users, String> usersStringCellEditEvent) {
+    public void onEditPes() {
+        checkEditPes();
         users user = table_info.getSelectionModel().getSelectedItem();
-        user.setPes(usersStringCellEditEvent.getNewValue());
+        oldVal = user.getPes();
+        checkPesEdit(sprPes);
+        if (cETN == true && sprPes.length() == 11 && sprPes.length() > 0) {
+            user.setPes(sprPes);
+        } else {
+            user.setPes(oldVal);
+        }
+        table_info.refresh();
     }
-    String spr;
+
     public void OnEditNrTel() {
-        checkEdit();
+        checkEditTel();
         users user = table_info.getSelectionModel().getSelectedItem();
-        user.setNr_tel(spr);
+        oldVal = user.getNr_tel();
+        checkTelEdit(sprTel);
+        if (cETN == true && sprTel.length() > 0) {
+            user.setNr_tel(sprTel);
+        } else {
+            user.setNr_tel(oldVal);
+        }
+        table_info.refresh();
+    }
+
+    public void onEditPosition(){
+        checkEditPosition();
+        users user = table_info.getSelectionModel().getSelectedItem();
+        oldVal = user.getTyp();
+        checkPositionEdit(sprPosition);
+        if (cETN == true && sprPosition.length() > 0) {
+            user.setTyp(sprPosition);
+        } else {
+            user.setTyp(oldVal);
+        }
         table_info.refresh();
     }
 
 
-//edycja nr telefonu działa
-    public void checkEdit(){
+
+    //edycja nr telefonu działa
+    public void checkEditPosition(){
+        TextInputDialog checkme = new TextInputDialog();
+        checkme.setTitle("Okno zmiany wartosci");
+        checkme.setHeaderText("Wpisz poprawne Stanowisko.");
+        checkme.setContentText("Wprowadz nowe Stanowisko:");
+
+        Optional<String> result = checkme.showAndWait();
+        if (result.isPresent()) {
+            sprPosition = result.get();
+        }
+    }
+
+    public void checkEditImie(){
+        TextInputDialog checkme = new TextInputDialog();
+        checkme.setTitle("Okno zmiany wartosci");
+        checkme.setHeaderText("Wpisz poprawne Imię.");
+        checkme.setContentText("Wprowadz nowe Imię:");
+
+        Optional<String> result = checkme.showAndWait();
+        if (result.isPresent()) {
+            sprImie = result.get();
+        }
+    }
+
+    public void checkEditTel() {
         TextInputDialog checkme = new TextInputDialog();
         checkme.setTitle("Okno zmiany wartosci");
         checkme.setHeaderText("Wpisz poprawny nr telefonu");
         checkme.setContentText("Wprowadz nowy nr telefonu:");
 
         Optional<String> result = checkme.showAndWait();
-        if (result.isPresent()){
-            spr = result.get();
+        if (result.isPresent()) {
+            sprTel = result.get();
+        }
+    }
+
+    public void checkTelEdit(String x) {
+        char[] checkTel = x.toCharArray();
+        for (int i = 0; i < x.length(); i++) {
+            if (checkTel[i] >= '0' && checkTel[i] <= '9') {
+                cETN = true;
+            } else {
+                cETN = false;
+                return;
+            }
+        }
+    }
+
+    public void checkEditId() {
+        TextInputDialog checkme = new TextInputDialog();
+        checkme.setTitle("Okno zmiany wartosci");
+        checkme.setHeaderText("Wpisz poprawny nr Id");
+        checkme.setContentText("Wprowadz nowy nr Id:");
+
+        Optional<String> result = checkme.showAndWait();
+        if (result.isPresent()) {
+            sprId = result.get();
+        }
+    }
+
+    public void checkEditPes() {
+        TextInputDialog checkme = new TextInputDialog();
+        checkme.setTitle("Okno zmiany wartosci");
+        checkme.setHeaderText("Wpisz poprawny nr Pesel");
+        checkme.setContentText("Wprowadz nowy nr Pesel:");
+
+        Optional<String> result = checkme.showAndWait();
+        if (result.isPresent()) {
+            sprPes = result.get();
         }
 
-}}
+    }
+
+    public void checkImieEdit(String x){
+        char[] checkImie = x.toCharArray();
+        for (int i = 0; i < x.length(); i++) {
+            if (checkImie[i] >= '0' && checkImie[i] <= '9') {
+                cETN = false;
+                return;
+            } else {
+                cETN = true;
+            }
+        }
+    }
+
+    public void checkPesEdit(String x) {
+        char[] checkPes = x.toCharArray();
+        for (int i = 0; i < x.length(); i++) {
+            if (checkPes[i] >= '0' && checkPes[i] <= '9') {
+                cETN = true;
+            } else {
+                cETN = false;
+                return;
+            }
+        }
+    }
+
+    public void checkEditSurname() {
+        TextInputDialog checkme = new TextInputDialog();
+        checkme.setTitle("Okno zmiany wartosci");
+        checkme.setHeaderText("Wpisz poprawne Nazwisko");
+        checkme.setContentText("Wprowadz nowye Nazwisko: ");
+
+        Optional<String> result = checkme.showAndWait();
+        if (result.isPresent()) {
+            sprSurname = result.get();
+        }
+    }
+
+    public void checkSurnameEdit(String x) {
+        char[] checkSurname = x.toCharArray();
+        for (int i = 0; i < x.length(); i++) {
+            if (checkSurname[i] >= '0' && checkSurname[i] <= '9') {
+                cETN = false;
+                return;
+            } else {
+                cETN = true;
+
+            }
+        }
+    }
+
+    public void checkIdEdit(String x) {
+        char[] checkid = x.toCharArray();
+        for (int i = 0; i < x.length(); i++) {
+            if (checkid[i] >= '0' && checkid[i] <= '9') {
+                cETN = true;
+            } else {
+                cETN = false;
+                return;
+            }
+        }
+    }
+
+    public void checkPositionEdit(String x){
+        char[] checkId = x.toCharArray();
+        for (int i = 0; i < x.length(); i++) {
+            if (checkId[i] >= '0' && checkId[i] <= '9') {
+                cETN = false;
+                return;
+            } else {
+                cETN = true;
+            }
+        }
+    }
+}
+
